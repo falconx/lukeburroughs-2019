6d28265782c91536b7e5e87969e6138fba67e13e
Matt Layton <mattlayt@gmail.com>
2019-11-10 11:37:12 +0000

    fix build

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import { BreakpointProvider } from '../components/Media';
 import Layout from '../components/layout/Layout';
 import SEO from '../components/Seo';
 
@@ -8,11 +9,13 @@ import theme from '../theme';
 
 const NotFoundPage = () => (
   <ThemeProvider theme={theme}>
-    <Layout>
-      <SEO title="404: Not found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist&hellip; the sadness.</p>
-    </Layout>
+  	<BreakpointProvider>
+      <Layout>
+        <SEO title="404: Not found" />
+        <h1>Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist&hellip; the sadness.</p>
+      </Layout>
+    </BreakpointProvider>
   </ThemeProvider>
 );
 
