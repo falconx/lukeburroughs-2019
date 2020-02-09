8dca9899397be1b1024883b4111a427e52229416
Matt Layton <mattlayt@gmail.com>
2020-02-09 14:39:04 +0000

    fix: apply link animations to mobile nav

--- src/components/layout/nav/CompactNavLinks.js
+++ src/components/layout/nav/CompactNavLinks.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+import CustomLink from '../../Link';
 
 const NavList = styled.ul``;
 
 const NavListItem = styled.li``;
 
-const NavLink = styled.a.attrs(props => ({
+const NavLink = styled(CustomLink).attrs(props => ({
+  as: Link,
   title: props.children,
 }))`
-  border-bottom: 1px solid transparent;
   font-size: 3.125rem;
   font-weight: 500;
   line-height: 1;
 
   &:hover,
   &:focus {
-    border-bottom-color: currentColor;
     color: rgba(255, 255, 255, 0.5) !important;
   }
 
